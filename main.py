4b3ae1199b251e0f9b31e361447137a7dd7da5b9
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-01-17 14:54:36 -0600

    setting up git

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+# learn to commit to the things you do
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
